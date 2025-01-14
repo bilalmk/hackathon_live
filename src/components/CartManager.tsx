43dc42dad2268a06eb7ca7fc0390a5a638c01216
bilalmk <bilalmk@gmail.com>
2025-01-14 14:50:40 +0500

    add customer form

--- src/components/CartManager.tsx
+++ src/components/CartManager.tsx
@@ -69,6 +69,43 @@ const CartManager = ({products}:{products:Product[]}) => {
                     </table>
                     <button className='w-full mt-4 py-2 bg-yellow-600 text-white text-sm font-medium rounded-md hover:bg-blue-700 transition'>CheckOut</button>
                 </div>
+                
+                    <div className="mt-8 bg-gray-100 p-6 rounded-lg shadow-md">
+                        <h2 className="text-lg font-semibold mb-4">Customer Information</h2>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-700">Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                value=""
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-700">Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value=""
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-700">Phone</label>
+                            <input
+                                type="phone"
+                                name="phone"
+                                value=""
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                            />
+                        </div>
+                        <button
+                            className="w-full py-2 bg-green-600 text-white text-sm font-medium rounded-md hover:bg-green-700 transition"
+                        >
+                            Submit Order
+                        </button>
+                    </div>
+                
             </div>
     </>
     
